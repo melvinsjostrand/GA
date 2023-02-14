162e982d3e658500761836b2437111bb733e822a
wiab03002 <wiab03002@utb.vaxjo.se>
2023-02-14 13:02:31 +0100

    Informationssida

--- HTML och CSS kod/js/Datum.js
+++ HTML och CSS kod/js/Datum.js
@@ -1,5 +1,5 @@
 function displayDateTime() {
-    const datetime = document.getElementById("datetime").value;
-    document.getElementById("info").innerHTML = `<p>Datum och Tid: ${datetime} </p>`;
-    
-  }
+  const datetime = document.getElementById("datetime").value;
+  document.getElementById("info").innerHTML = `<p>Datum och Tid: ${datetime} </p>`;
+  
+}
---
+++ HTML och CSS kod/js/Informationssida.js
@@ -0,0 +1,46 @@
+let json =[
+        {org:"Omsfritid",dag:"230214",tid:"12:00", info:"Pizza"},
+        {org:"Omsfritid",dag:"230214",tid:"14:00", info:"Bowling"},
+
+];
+
+let time;
+
+function init(){
+    time = document.getElementById("time");
+    console.log(time);
+
+    json.forEach(element=>{
+        console.log(element);
+        time.innerHTML = element.tid + " " + element.org;
+
+        createSection(element);
+    })
+
+}
+
+window.onload = init;
+
+function createSection(el){
+    let sections = document.getElementsByClassName("gridsection2");
+    console.log(sections[0]);
+
+    let section = document.createElement("section");
+  
+    let h3 = document.createElement("h3");
+    h3.innerHTML = el.org;
+    console.log(h3);
+
+    section.appendChild(h3);
+   
+
+    let paragraph = document.createElement("p");
+    let t = el.tid + " " + el.info;
+    let text = document.createTextNode(t);
+    paragraph.appendChild(text);
+    console.log(paragraph);
+    section.appendChild(paragraph);
+    console.log(section);
+
+    sections[0].appendChild(section);
+}
