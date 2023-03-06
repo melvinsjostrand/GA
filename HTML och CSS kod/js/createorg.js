b42d631392e8b2ba47825aab0c946fa5d5e2843d
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-03-06 20:37:30 +0100

    fixes

--- HTML och CSS kod/js/createorg.js
+++ HTML och CSS kod/js/createorg.js
@@ -18,12 +18,11 @@ window.onload = init;
 function getFormData(){
     let org = form.elements.createorg.value;
     let description = form.elements.desc.value;
-   let image = form.elements.fileinput.files;
+    let image = form.elements.fileinput.files;
 
-    console.log(description);
-    console.log(org);
-console.log(image);
-
+        console.log(description);
+        console.log(org);
+        console.log(image);
 }
 
 
@@ -35,15 +34,16 @@ fileInput.addEventListener("change", e =>{
     const file = fileInput.files[0];
     const reader = new FileReader();
 
-    reader.addEventListener("load", () => {
-   console.log(reader.result);
+        reader.addEventListener("load", () => {
+        console.log(reader.result);
 
     });
 
-    reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
 });
 
-/*async function postFetch(getFormData , token , json){
+
+async function postFetch(getFormData , token , json){
     const reponse = await fetch(getFormData ,{
         method:"post",
         mode:"cors",
@@ -52,11 +52,11 @@ fileInput.addEventListener("change", e =>{
             "authorazation":token
         },
         body:json.stringify({
-        Image:"string : base64,
-        org:"orgdata,
+        Image:"string : base64",
+        org:"orgdata",
         description:"descriptiondata"
         })
     })
     const json = await return respone.json();
     
-}*/
+}
