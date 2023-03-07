be912e37c5d56cdcb92ff7ed1850d23a8bc40751
Alexsept177 <alan03002@utb.vaxjo.se>
2023-03-07 14:24:38 +0100

    index sida har en uppload button till createorg

--- HTML och CSS kod/js/index.js
+++ HTML och CSS kod/js/index.js
@@ -1,5 +1,10 @@
-function createSection() {
-    let section = document.createElement("section");
-    let paragraph = document.createElement("em");
-    
-}
+// function createSection() {
+//     let section = document.createElement("section");
+//     let paragraph = document.createElement("em");
+// }
+
+let uppload = document.getElementById("upploadbutton");
+uppload.addEventListener("click",event=> {
+    location.href = "createorg.html";
+})
+
