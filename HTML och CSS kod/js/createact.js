20694c434c8105cb4205aa97a6eefddbbea42aa6
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-03-07 17:01:36 +0100

    klar med skapa aktivitet tror jag

--- HTML och CSS kod/js/createact.js
+++ HTML och CSS kod/js/createact.js
@@ -1,36 +1,5 @@
-const formEl = document.querySelector('.form');
-
-formEl.addEventListener('submit', event => {
-    event.preventDefault();
-
-    const formdata = new FormData(formEl);
-    const data = new URLSearchParams(formdata)
-    
-    fetch('api', {
-        method: 'POST',
-        body: data
-    }).then(res => res.json())
-    .then (data => console.log(data))
-    .catch(error => console.log(error))
-
-});
-
 let form;
-//const backurl = "https://omsapp.te-vxo.se/index.html";
-const serverurl = "";
-
-async function postFetch(path, token , JSON){
-    const response = await fetch(path ,{
-    method: "post",
-    mode:"cors",
-    headers:{
-    "content-type":application,
-    "authorization":token
-    },
-    body:JSON.stringify(JSON)
-    });
-    return response.status;
-}
+const backurl = "https://omsapp.te-vxo.se/index.html";
 
 function init(){
 
@@ -38,35 +7,53 @@ function init(){
 
     form.addEventListener("submit", event=>{
         getFormData();
+        //location.href = backurl;
         event.preventDefault();
     })
+
 }
 
 window.onload = init;
 
-function getFormData(){
+async function getFormData(){
     let location = form.elements.location.value;
-    let starttime = form.elements.start_time.value;
-    let endtime = form.elements.end_time.value;
-    let information = form.elements.upload_information.value;
+    let start_time = form.elements.start_time.value;
+    let upload_information = form.elements.upload_information.value;
+    let end_time = form.elements.end_time.value;
+    //let image = form.elements.fileinput.files;
+
+        console.log(start_time);
+        console.log(end_time);
+        console.log(location);
+        console.log(upload_information);
+        Json={            
+            "location":location,
+            "start_time":start_time,
+
+        }
+
+        console.log(Json);
     
-    console.log(location);
-    console.log(starttime);
-    console.log(endtime);
-    console.log(information)
+        let status = await postFetch(Json);
+
+        console.log(status);
+
 }
 
-let JSON = {
-    "act": "Titel",
-    "place": location,
-    "info": information,
-    "starttime": {
-        "date":starttime ,
-        "time": starttime
-    },
-    "endtime": {
-        "date": endtime,
-        "time": endtime
-    }
 
+async function postFetch(json, /*token*/){
+    let path = "https://omsorgapiapi.azure-api.net/Activity";
+
+    const response = await fetch(path ,{
+        method:"post",
+        mode:"cors",
+        Headers:{
+            "content-type":"application/json"
+            /*"authorazation":token*/
+        },
+        body:JSON.stringify(json)
+    })
+
+   return response.status;
+    
 }
