498ecca835bb0b5f159d240bd07c0622c9607ef5
Alexsept177 <alan03002@utb.vaxjo.se>
2023-03-08 13:20:12 +0100

    på börjat med json obiekt och index sidan fetch

--- HTML och CSS kod/js/index.js
+++ HTML och CSS kod/js/index.js
@@ -1,10 +1,53 @@
-// function createSection() {
-//     let section = document.createElement("section");
-//     let paragraph = document.createElement("em");
-// }
+let organisation;
+
+
+let json = [
+    {
+        "orgId": "",
+        "org": "OmsFritid",
+        "image": "",
+        "favorite": ""
+    }
+]
+
+
+function unit(){
+    createOrg();
+}
+// window.onload = init;
+
+
+
+async function getOrg(){
+    organisation = document.getElementById("organisation")
+
+    let path = "https://omsorgapiapi.azure-api.net/Organization" + org ;
+
+    createorg = await getOrgFetch(path);
+    console.log(createorg);
+}
+
+function createOrg(cont) {
+    let Section = document.createElement("section");
+    let Title = document.createElement("em");
+
+    createorg[cont+1].Organization.foreach(element=>{
+        let Title = document.createElement("em");
+        let OrgName = document.createTextNode(element.org);
+        Title.appendChild(OrgName);
+        Section.appendChild(Title);
+    })
+}
+
+async function getOrgFetch(path){
+    let response = await fetch(path);
+    let json = await response.json();
+    return json;
+}
+
+
 
 let uppload = document.getElementById("upploadbutton");
 uppload.addEventListener("click",event=> {
     location.href = "createorg.html";
 })
-
