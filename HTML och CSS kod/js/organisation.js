0a013609cb953f5ea936faf6fef53d8df80a3724
Alex <alan03002@utb.vaxjo.se>
2023-03-17 11:19:29 +0100

    organisation json objekt title funkar

--- HTML och CSS kod/js/organisation.js
+++ HTML och CSS kod/js/organisation.js
@@ -3,7 +3,9 @@ creatact.addEventListener("click",event=> {
     location.href = "uploadinformation.html";
 })
  
-let json = [{
+let activities;
+
+let json = {
     "org": "test",
     "description": "Alla blaakasjdjfaliunvipartbauih vpnautilsef",
     "image": "string",
@@ -24,11 +26,237 @@ let json = [{
                 "date": "string",
                 "time": "string"
             }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
+        },
+        {
+            "actId": "int",
+            "org": "string",
+            "act": "string",
+            "info": "string",
+            "place": "string",
+            "start": {
+                "date": "string",
+                "time": "string"
+            },
+            "end": {
+                "date": "string",
+                "time": "string"
+            }
         }
     ]
-}]
+}
 
 function init() {
+    activities = document.getElementById("text");
     getjson();
 
 }
@@ -37,18 +265,35 @@ window.onload = init;
 
 
 function getjson() {
-    json.forEach(element => {
-        getName(element);
+    getName();
+
+    json.activities.forEach(element=>{
+        createActivity(element);
     })
+    
 }
 
 
-function getName(element) {
+function getName() {
     let Orgpos = document.getElementById("organisation-name");
+    let Orgpos1 = document.getElementById("Organisation-description");
     let OrgNameTag = document.getElementsByTagName("h2");
-    let OrgName = document.createTextNode(element.org);
-    OrgNameTag.appendChild(OrgName);
-    Orgpos.appendChild(OrgNameTag);
+    let OrgName = document.createTextNode(json.org);
+    OrgNameTag[0].appendChild(OrgName);
+    Orgpos.appendChild(OrgNameTag[0]);
+    console.log(Orgpos);
+
+    OrgName = document.createTextNode(json.org);
+    OrgNameTag[1].appendChild(OrgName);
+    Orgpos1.appendChild(OrgNameTag[1]);
+}
+
+function createActivity(el){
+    let paragraph = document.createElement("p");
+    let text = el.start.date + " " + el.info; 
+    let tN = document.createTextNode(text);
+    paragraph.appendChild(tN);
+    activities.appendChild(paragraph);
 }
 
 
