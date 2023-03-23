c695ca0d9ed2debabd16e5329155a2f91328450c
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-03-23 11:57:06 +0100

    testar runt med att skapa konto

--- HTML och CSS kod/js/account.js
+++ HTML och CSS kod/js/account.js
@@ -19,7 +19,62 @@ iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
 });
 
-function myFunction() {
-  let popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+
+let form;
+
+function init(){
+
+    form = document.querySelector("form");
+
+    form.addEventListener("submit", event=>{
+        getFormData();
+        //location.href = backurl;
+        event.preventDefault();
+    })
+
+}
+
+window.onload = init;
+
+async function getFormData(){
+    let createaccusername = form.elements.createaccusername.value;
+    let createaccemail = form.elements.createaccemail.value;
+    let createaccpassword = form.elements.createaccpassword.value;
+    createaccpassword =password;
+    createaccusername = username;
+
+    console.log(createaccemail);
+    console.log(createaccpassword);
+    console.log(createaccusername);
+
+
+Json = {
+"mail":createaccemail
+}
+
+
+        console.log(Json);
+    
+        let status = await postFetch(Json);
+
+        console.log(status);
+
+}
+
+
+async function postFetch(json, /*token*/){
+    let path = "https://omsorgapiapi.azure-api.net//User";
+
+    const response = await fetch(path ,{
+        method:"post",
+        mode:"cors",
+        Headers:{
+            "content-type":"application/json"
+            /*"authorazation":"Basic" + btoa(username:password)*/
+        },
+        body:JSON.stringify(json)
+    })
+
+   return response.status;
+    
 }
