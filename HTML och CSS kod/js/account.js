213d90b303e3e7b8cb34889429135e4745423c30
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2023-03-24 09:20:57 +0100

    account sida

--- HTML och CSS kod/js/account.js
+++ HTML och CSS kod/js/account.js
@@ -1,8 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
+
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -12,12 +11,6 @@ loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup');
-});
-iconClose.addEventListener('click', ()=>{
-    wrapper.classList.remove('active-popup');
-});
 
 
 let form;
