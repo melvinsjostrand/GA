ccfbe92fbd90e92ef0b13cf6b18ff622e9b8e706
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2023-03-24 11:40:31 +0100

    skapa konto klart

---
+++ HTML och CSS kod/js/createaccount.js
@@ -0,0 +1,60 @@
+let form;
+
+function init(){
+
+    form = document.querySelector("form");
+
+    form.addEventListener("submit", event=>{
+        getFormData();
+        //location.href = backurl;
+        event.preventDefault();
+    })
+
+}
+
+window.onload = init;
+
+async function getFormData(){
+    let username = form.elements.createaccusername.value;
+    let email = form.elements.createaccemail.value;
+    let password = form.elements.createaccpassword.value;
+    let phonenumber = form.elements.createaccphonenumber.value;
+
+  
+    console.log(username);
+    console.log(email);
+    console.log(password);
+    console.log(phonenumber);
+
+
+    Json = {
+    "mail":email,
+    "phone":phonenumber
+    }
+
+
+            console.log(Json);
+        
+            let status = await postFetch(Json);
+
+            console.log(status);
+
+    }
+
+
+async function postFetch(json, /*token*/){
+    let path = "https://omsorgapiapi.azure-api.net/User";
+
+    const response = await fetch(path ,{
+        method:"post",
+        mode:"cors",
+        Headers:{
+            "content-type":"application/json"
+          /*  "Authorization": "Basic: " + btoa(username:password) */
+        },
+        body:JSON.stringify(json)
+    })
+
+   return response.status;
+    
+}
